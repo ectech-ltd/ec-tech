231be0549cc4ea600f71b7868756ea92c94cce2b
Hieu Vu <vuhieu@outlook.com>
2023-05-30 10:23:53 +0700

    Update render richtext

---
+++ components/NotionRichtext.tsx
@@ -0,0 +1,47 @@
+import { RichTextItemResponse } from '@notionhq/client/build/src/api-endpoints'
+import { FC, Fragment } from 'react'
+import cn from 'classnames'
+
+interface Props {
+  items?: RichTextItemResponse[]
+}
+
+const NotionRichText: FC<Props> = ({ items = [] }) => {
+  return (
+    <>
+      {items.map((richText, i) => {
+        const className = cn({
+          'font-bold': richText.annotations.bold,
+          italic: richText.annotations.italic,
+          underline: richText.annotations.underline,
+          'line-through': richText.annotations.strikethrough,
+        })
+        if (richText.href) {
+          return (
+            <a
+              key={i}
+              href={richText.href}
+              className={cn(className, 'underline')}
+              target="_blank"
+              rel="noopener noreferer noreferrer"
+            >
+              {richText.plain_text}
+            </a>
+          )
+        }
+        return (
+          <span key={i} className={className}>
+            {richText.plain_text.split('\n').map((text, i) => (
+              <Fragment key={i}>
+                {i > 0 && <br />}
+                {text}
+              </Fragment>
+            ))}
+          </span>
+        )
+      })}
+    </>
+  )
+}
+
+export default NotionRichText
--- components/pages/product-details/ProductDetailHeader.tsx
+++ components/pages/product-details/ProductDetailHeader.tsx
@@ -1,5 +1,5 @@
+import NotionRichText from '@/components/NotionRichtext'
 import { IProduct } from '@/lib/notion/products'
-import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
 
 export default function ProductDetailHeader({ data }: { data: IProduct }) {
   return (
@@ -10,21 +10,11 @@ export default function ProductDetailHeader({ data }: { data: IProduct }) {
       <div>{data.properties.Rate?.select?.name}</div>
       <div className="divide-y">
         <div className="py-2">
-          <ReactMarkdown>
-            {data.properties.Summary?.rich_text[0]?.plain_text.replaceAll(
-              '\n',
-              '\n\n\n',
-            )}
-          </ReactMarkdown>
+          <NotionRichText items={data.properties.Summary?.rich_text as any} />
         </div>
         <div className="py-2">
           <h2 className="text-xl font-semibold">THÔNG TIN SẢN PHẨM</h2>
-          <ReactMarkdown>
-            {data.properties.Spec?.rich_text[0]?.plain_text.replaceAll(
-              '\n',
-              '\n\n',
-            )}
-          </ReactMarkdown>
+          <NotionRichText items={data.properties.Spec?.rich_text as any} />
         </div>
       </div>
     </div>
